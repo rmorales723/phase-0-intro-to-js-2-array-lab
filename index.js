a2783fc6d2e558d3a61f595b828b2ee99198d300
robert morales <rmoralfit@gmail.com>
2021-03-10 21:11:12 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+
+function prependCat(name){
+    return [name, ...cats];
+}
+
+function removeLastCat(name){
+    return cats.slice(name);
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
