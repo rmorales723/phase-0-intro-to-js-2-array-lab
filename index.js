ce041dee19174e780f6944d4c4e8259786f392b8
robert morales <rmoralfit@gmail.com>
2021-03-22 16:54:31 -0400

    commit tree 211de8089be7fa7423569da422af320f8094d81b

--- index.js
+++ index.js
@@ -4,40 +4,24 @@ const cats = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name){
     cats.push(name);
 }
-
 function destructivelyPrependCat(name){
     cats.unshift(name);
 }
-
 function destructivelyRemoveLastCat(name){
     cats.pop(name);
 }
-
 function destructivelyRemoveFirstCat(name){
     cats.shift(name);
 }
-
 function appendCat(name){
-    cats.shift(name);
+    return[...cats, name];
 }
-
-function appendCat(name){
-    return [...cats, name];
-}
-
-
 function prependCat(name){
-    return [name, ...cats];
+    return[name, ...cats];
 }
-
 function removeLastCat(name){
-    return cats.slice(name);
+    return cats.slice(0, cats.length - 1);
 }
-
-function removeLastCat(){
-    return cats.slice(0, cats.length -1);
-}
-
-function removeFirstCat(){
+function removeFirstCat(name){
     return cats.slice(1);
 }
